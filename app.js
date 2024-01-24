7429584a6788b64caed1ee63220dd0efbbbf2eee
Prasad More <prasadmoremain@gmail.com>
2024-01-24 17:56:04 +0530

    Created Test Ranking Table

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-let covid = new XMLHttpRequest()
+let cricket = new XMLHttpRequest()
 
 let arrDisData = []
 let newArrs = []
 
 
-covid.open("GET","cric.JSON",true)
+cricket.open("GET","cric.JSON",true)
 
-covid.send()
+cricket.send()
 
-covid.onload = function(){
+cricket.onload = function(){
     let response = this.responseText;
     let cricket = JSON.parse(response)
-    console.log(cricket.data)
+    
     let test = cricket.data[0].team
-
+    console.log(test)
     let table = document.querySelector('table')
+    table.innerHTML="<tr><td>Ranking</td><td>Country Name</td><td>Rating</td><td>Flag</td></tr>"
+
 
     for(let i=0;i<=test.length;i++){
-
+        table.innerHTML+="<tr><td>"+test[i].position+"</td><td>"+test[i].name+"</td><td>"+test[i].ranking.rating+"</td><td><img src="+test[i].image_path+"></td><tr>"
     }
 
 }
